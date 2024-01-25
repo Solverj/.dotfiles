31ebc6b117b5b9f66d6a04aa533f0479fb350f3f
sølve <solve.sthor@gmail.com>
2024-01-25 14:16:34 +0100

    Added all dotfiles to .dotfiles

---
+++ nvim/README.md
@@ -0,0 +1,1 @@
+Requires nvim 0.10.x for vim.lsp support
---
+++ symlink.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Same as ln -sb (backup if file exists)
+# but mac compatible
+# note that the second argument must be a directory.
+ln_sb() {
+	file_relpath="$1"
+	dest_dir="$2"
+
+	basename="$(basename "$file_relpath")"
+	dest_file="$dest_dir/$basename"
+
+	mkdir -p "$dest_dir"
+	if [[ -f "$dest_file" ]] || [[ -d "$dest_file" ]]; then
+		echo "Backing up $dest_file to ${dest_file}~"
+		if [[ -f "$dest_file"~ ]] || [[ -d "$dest_file"~ ]]; then
+			\rm -rf "${dest_file}"~
+		fi
+		mv "$dest_file" "${dest_file}"~
+	fi
+
+	ln -s "$CURRENT_DIR/$file_relpath" "$dest_dir"
+}
+
+ln_sb nvim ~/.config
+ln_sb oh-my-zsh/.zshrc ~
+# Installing at ~/.oh-my-zsh will make it difficult to manage oh-my-zsh installation.
+ln_sb i3 ~/.config
+ln_sb alacritty ~/.config
